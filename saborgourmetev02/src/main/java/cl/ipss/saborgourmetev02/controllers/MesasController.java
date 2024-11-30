df9f5ee2d2053f67c7d1d9cc8571a3dc5bad494c
Laura Figueroa <laufigueroasalas@gmail.com>
2024-11-30 12:46:37 -0300

    dev: "Added styles for index.html"

--- saborgourmetev02/src/main/java/cl/ipss/saborgourmetev02/controllers/MesasController.java
+++ saborgourmetev02/src/main/java/cl/ipss/saborgourmetev02/controllers/MesasController.java
@@ -17,36 +17,36 @@ public class MesasController {
     @Autowired
     private MesasService mesasService;
 
-    //Listar Mesas
-@GetMapping
-public String listarMesas(Model model) {
-    model.addAttribute("titulo", "Lista de Mesas");
-    model.addAttribute("mesas", mesasService.listarTodas());
-    return "mesas-listar";
-}
+    // Listar Mesas
+    @GetMapping
+    public String listarMesas(Model model) {
+        model.addAttribute("titulo", "Lista de Mesas");
+        model.addAttribute("mesas", mesasService.listarTodas());
+        return "mesas-listar";
+    }
 
-    //Nueva Mesa 
- @GetMapping("/nuevo")
-      public String nuevaReserva(Model model) {
-          model.addAttribute("mesas", new Mesas());
-          return "mesas-agregar";
-      }
+    // Nueva Mesa
+    @GetMapping("/nuevo")
+    public String nuevaReserva(Model model) {
+        model.addAttribute("mesas", new Mesas());
+        return "mesas-agregar";
+    }
 
-    //Guardar Mesa
-     @PostMapping("/guardar")
+    // Guardar Mesa
+    @PostMapping("/guardar")
     public String guardarMesas(Mesas mesas) {
         mesasService.guardar(mesas);
         return "redirect:/mesas";
     }
 
-    //Editar Mesas 
+    // Editar Mesas
     @GetMapping("/editar/{id}")
-      public String editarMesas(@PathVariable Long id, Model model) {
-          model.addAttribute("mesas", mesasService.obtenerPorId(id));
-          return "mesas-agregar";
+    public String editarMesas(@PathVariable Long id, Model model) {
+        model.addAttribute("mesas", mesasService.obtenerPorId(id));
+        return "mesas-agregar";
     }
 
-    //Eliminar Mesas
+    // Eliminar Mesas
     @GetMapping("/eliminar/{id}")
     public String eliminarMesas(@PathVariable Long id) {
         mesasService.eliminar(id);
--- saborgourmetev02/src/main/java/cl/ipss/saborgourmetev02/controllers/ReservasController.java
+++ saborgourmetev02/src/main/java/cl/ipss/saborgourmetev02/controllers/ReservasController.java
@@ -16,47 +16,46 @@ import cl.ipss.saborgourmetev02.services.ReservasService;
 @RequestMapping("/reservas")
 public class ReservasController {
 
-    @Autowired
-    private ReservasService reservasService;
-    @Autowired
-    private MesasService mesasService;
+  @Autowired
+  private ReservasService reservasService;
+  @Autowired
+  private MesasService mesasService;
 
-    //Listar Reservas
-     @GetMapping
-    public String listarReservas(Model model) {
-        model.addAttribute("titulo", "Lista de Reservas");
-        model.addAttribute("reservas", reservasService.listarTodas());
-        return "reservas-listar";
-    }
+  // Listar Reservas
+  @GetMapping
+  public String listarReservas(Model model) {
+    model.addAttribute("titulo", "Lista de Reservas");
+    model.addAttribute("reservas", reservasService.listarTodas());
+    return "reservas-listar";
+  }
 
-    //Nueva Reserva
-     @GetMapping("/nuevo")
-    public String nuevaReserva(Model model) {
-        model.addAttribute("reservas", new Reservas());
-        model.addAttribute("mesas", mesasService.listarTodas());
-        return "reservas-agregar";
-    }
+  // Nueva Reserva
+  @GetMapping("/nuevo")
+  public String nuevaReserva(Model model) {
+    model.addAttribute("reservas", new Reservas());
+    model.addAttribute("mesas", mesasService.listarTodas());
+    return "reservas-agregar";
+  }
 
-    //Guardar Reserva
-      @PostMapping("/guardar")
-      public String guardarReserva(Reservas serie, RedirectAttributes redirectAttributes) {
-          reservasService.guardar(serie);
-          redirectAttributes.addFlashAttribute("msg", "Reserva guardada con éxito!");
-          return "redirect:/reservas";
-        }
+  // Guardar Reserva
+  @PostMapping("/guardar")
+  public String guardarReserva(Reservas serie, RedirectAttributes redirectAttributes) {
+    reservasService.guardar(serie);
+    redirectAttributes.addFlashAttribute("msg", "Reserva guardada con éxito!");
+    return "redirect:/reservas";
+  }
 
-    // Editar Reserva    
-      @GetMapping("/editar/{id}")
-      public String editarReserva(@PathVariable Long id, Model model) {
-          model.addAttribute("serie", reservasService.obtenerPorId(id));
-          return "reservas-agregar";
-      }
-  
-      // Eliminar Reserva
-      @GetMapping("/eliminar/{id}")
-      public String eliminarReserva(@PathVariable Long id) {
-        reservasService.eliminar(id);
-          return "redirect:/reservas";
-      }
+  // Editar Reserva
+  @GetMapping("/editar/{id}")
+  public String editarReserva(@PathVariable Long id, Model model) {
+    model.addAttribute("serie", reservasService.obtenerPorId(id));
+    return "reservas-agregar";
+  }
+
+  // Eliminar Reserva
+  @GetMapping("/eliminar/{id}")
+  public String eliminarReserva(@PathVariable Long id) {
+    reservasService.eliminar(id);
+    return "redirect:/reservas";
+  }
 }
-
